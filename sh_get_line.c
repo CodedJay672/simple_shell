6e03083ac45b5726aafa6c510a9c9d47427ed6f2
Codedjay672 <emmajoel6456@gmail.com>
2022-09-07 23:25:57 +0000

    updating shell

--- sh_get_line.c
+++ sh_get_line.c
@@ -1,33 +1,84 @@
 #include "main.h"
 
 /**
- * get_help - function that retrieves help messages according builtin
- * @datash: data structure (args and input)
- * Return: Return 0
-*/
-int get_help(data_shell *datash)
+ * bring_line - assigns the line var for get_line
+ * @lineptr: Buffer that store the input str
+ * @buffer: str that is been called to line
+ * @n: size of line
+ * @j: size of buffer
+ */
+void bring_line(char **lineptr, size_t *n, char *buffer, size_t j)
 {
 
-	if (datash->args[1] == 0)
-		aux_help_general();
-	else if (_strcmp(datash->args[1], "setenv") == 0)
-		aux_help_setenv();
-	else if (_strcmp(datash->args[1], "env") == 0)
-		aux_help_env();
-	else if (_strcmp(datash->args[1], "unsetenv") == 0)
-		aux_help_unsetenv();
-	else if (_strcmp(datash->args[1], "help") == 0)
-		aux_help();
-	else if (_strcmp(datash->args[1], "exit") == 0)
-		aux_help_exit();
-	else if (_strcmp(datash->args[1], "cd") == 0)
-		aux_help_cd();
-	else if (_strcmp(datash->args[1], "alias") == 0)
-		aux_help_alias();
-	else
-		write(STDERR_FILENO, datash->args[0],
-		      _strlen(datash->args[0]));
+	if (*lineptr == NULL)
+	{
+		if  (j > BUFSIZE)
+			*n = j;
 
-	datash->status = 0;
-	return (1);
+		else
+			*n = BUFSIZE;
+		*lineptr = buffer;
+	}
+	else if (*n < j)
+	{
+		if (j > BUFSIZE)
+			*n = j;
+		else
+			*n = BUFSIZE;
+		*lineptr = buffer;
+	}
+	else
+	{
+		_strcpy(*lineptr, buffer);
+		free(buffer);
+	}
+}
+/**
+ * get_line - Read inpt from stream
+ * @lineptr: buffer that stores the input
+ * @n: size of lineptr
+ * @stream: stream to read from
+ * Return: The number of bytes
+ */
+ssize_t get_line(char **lineptr, size_t *n, FILE *stream)
+{
+	int i;
+	static ssize_t input;
+	ssize_t retval;
+	char *buffer;
+	char t = 'z';
+
+	if (input == 0)
+		fflush(stream);
+	else
+		return (-1);
+	input = 0;
+
+	buffer = malloc(sizeof(char) * BUFSIZE);
+	if (buffer == 0)
+		return (-1);
+	while (t != '\n')
+	{
+		i = read(STDIN_FILENO, &t, 1);
+		if (i == -1 || (i == 0 && input == 0))
+		{
+			free(buffer);
+			return (-1);
+		}
+		if (i == 0 && input != 0)
+		{
+			input++;
+			break;
+		}
+		if (input >= BUFSIZE)
+			buffer = _realloc(buffer, input, input + 1);
+		buffer[input] = t;
+		input++;
+	}
+	buffer[input] = '\0';
+	bring_line(lineptr, n, buffer, input);
+	retval = input;
+	if (i != 0)
+		input = 0;
+	return (retval);
 }
